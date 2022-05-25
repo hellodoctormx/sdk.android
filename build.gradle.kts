45d659920078b53c5c29fa3398fd1fcf2fe93d8c
Daniel Sarfati <daniel@hellodoctor.com.mx>
2022-05-25 00:56:45 -0500

    * stable build point

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id ("com.android.library") version "7.2.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id ("org.jetbrains.kotlin.multiplatform") version "1.6.10"
+    id ("org.jetbrains.kotlin.plugin.serialization") version "1.6.10"
+    id ("com.google.gms.google-services") version "4.3.10"
+}
+
+kotlin {
+    jvm()
+}
