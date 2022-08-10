397a1d8c1386cda2b975273785adcf6a4e12bcbe
Daniel Sarfati <daniel@hellodoctor.com.mx>
2022-08-10 02:32:04 -0500

    better colors

--- hellodoctor-native-sdk/src/main/java/com/hellodoctormx/sdk/ui/theme/Color.kt
+++ hellodoctor-native-sdk/src/main/java/com/hellodoctormx/sdk/ui/theme/Color.kt
@@ -2,14 +2,14 @@ package com.hellodoctormx.sdk.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Blue200 = Color(0xFF82c6ff)
-val Blue500 = Color(0xFF0062B2)
-val Blue700 = Color(0xFF00294b)
-val Green200 = Color(0xFF16B00E)
+val Blue200 = Color(0xFF45AAFF)
+val Blue500 = Color(0xFF096FC3)
+val Blue700 = Color(0xFF064579)
+val Green200 = Color(0xFF198754)
 val Gray500 = Color(0xFF555555)
 val Gray900 = Color(0xFF222222)
 val Red200 = Color(0xFFE67573)
-val Red500 = Color(0xFFC52723)
+val Red500 = Color(0xFFbd2f2b)
 val Purple200 = Color(0xFF8b768b)
 val Purple500 = Color(0xFF5D4760)
 val Purple700 = Color(0xFF4a2478)
